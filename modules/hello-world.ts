85e504c42d62696a7aad6c5c98e18da55152f77c
aabedraba <aabedraba@gmail.com>
2023-08-05 15:34:39 +0400

    use vscode

--- modules/hello-world.ts
+++ modules/hello-world.ts
@@ -1,11 +1,11 @@
 import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
 
 export default async function (request: ZuploRequest, context: ZuploContext) {
-  const formData = await request.formData()
+  const formData = await request.formData();
 
-  formData.forEach(data => {
+  formData.forEach((data) => {
     context.log.info(data);
-  })
+  });
 
   return "What zup?";
 }
