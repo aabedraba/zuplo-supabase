8b456b7589cbd6fb35479128f041011301b1a0c3
aabedraba <aabedraba@gmail.com>
2023-08-05 15:40:52 +0400

    use different form parsing

--- modules/hello-world.ts
+++ modules/hello-world.ts
@@ -3,9 +3,9 @@ import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
 export default async function (request: ZuploRequest, context: ZuploContext) {
   const formData = await request.formData();
 
-  formData.forEach((data) => {
-    context.log.info(data);
-  });
+  for (const [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
 
   return "What zup?";
 }
